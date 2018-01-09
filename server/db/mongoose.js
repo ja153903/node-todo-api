2d81e59f8376e6c97a29c9c7630043f111fad055
ja153903 <jaimeabbariao@gmail.com>
2018-01-09 15:40:04 -0500

    syntax changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // set it up to use promises instead of callbacks
 
-mongo_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
+mongo_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.connect(mongo_uri);
 
 module.exports = { mongoose }
